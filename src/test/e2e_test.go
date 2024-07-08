f8058871b94c4df573a42c6353242c83b12c3eaf
siddhant <siddhantkumarupmanyu@gmail.com>
2024-07-09 00:33:36 +0530

    extract to e2e

---
+++ src/test/e2e_test.go
@@ -0,0 +1,32 @@
+package test
+
+import (
+	"gRunner/src/pkg/runner"
+	"testing"
+)
+
+func Test_TestRunner(t *testing.T) {
+	r := runner.NewTestsRunner[string](t)
+	r.Setup(setup)
+	r.Teardown(teardown)
+	r.Add(sampleTest)
+	r.Run()
+}
+
+func setup(t *testing.T) string {
+	// do setup
+	return "setup-data"
+}
+
+func teardown(t *testing.T, extra string) {
+	// do teardown
+	t.Logf("teardown got extra '%s'", extra)
+}
+
+func sampleTest(t *testing.T, extra string) {
+	if extra != "setup-data" {
+		t.Fatalf("wrong value, expected \"setup-data\", got \"%s\"", extra)
+	}
+	t.Logf("should print function name '%s' in test runner", "sampleTest")
+	t.Logf("sampleTest got extra '%s'", extra)
+}
--- src/test/runner/TestsRunner_test.go
+++ src/test/runner/TestsRunner_test.go
@@ -72,30 +72,3 @@ func Test_Extra(tm *testing.T) {
 // teardownCalledEvenIfFatal
 
 // support for one time setup and teardown
-
-// Usage
-func Test_TestRunner(t *testing.T) {
-	r := runner.NewTestsRunner[string](t)
-	r.Setup(setup)
-	r.Teardown(teardown)
-	r.Add(sampleTest)
-	r.Run()
-}
-
-func setup(t *testing.T) string {
-	// do setup
-	return "setup-data"
-}
-
-func teardown(t *testing.T, extra string) {
-	// do teardown
-	t.Logf("teardown got extra '%s'", extra)
-}
-
-func sampleTest(t *testing.T, extra string) {
-	if extra != "setup-data" {
-		t.Fatalf("wrong value, expected \"setup-data\", got \"%s\"", extra)
-	}
-	t.Logf("should print function name '%s' in test runner", "sampleTest")
-	t.Logf("sampleTest got extra '%s'", extra)
-}
