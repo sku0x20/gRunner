ef33dcd0da38f0f2614c770ba444ecdf33f86d3a
siddhant <siddhantkumarupmanyu@gmail.com>
2024-09-13 16:57:19 +0530

    refactor tests

--- src/test/utils/emptyinit_test.go
+++ src/test/utils/emptyinit_test.go
@@ -5,24 +5,25 @@ import (
 	"testing"
 )
 
-func Test_Int(t *testing.T) {
+func Test_Value(t *testing.T) {
 	e := utils.EmptyInit[int]()
 	if e != 0 {
 		t.Fatalf("e != 0")
 	}
 }
 
-func Test_PointerInt(t *testing.T) {
+func Test_Pointer(t *testing.T) {
 	e := utils.EmptyInit[*int]()
-	if e == nil {
-		t.Fatalf("e == nil")
+	*e = 1000
+	if *e != 1000 {
+		t.Fatalf("*e != 1000")
 	}
 }
 
-func Test_SliceInt(t *testing.T) {
+func Test_Slice(t *testing.T) {
 	e := utils.EmptyInit[[]int]()
 	e = append(e, 12)
 	if e[0] != 12 {
-		t.Fatalf("e == nil")
+		t.Fatalf("e[0] != 12")
 	}
 }
