87c57d6ef412eb1a82962f26e539aae746404137
siddhant <siddhantkumarupmanyu@gmail.com>
2024-09-13 16:47:23 +0530

    replace if with switch

--- src/pkg/utils/emptyinit.go
+++ src/pkg/utils/emptyinit.go
@@ -6,10 +6,12 @@ import (
 
 func EmptyInit[E any]() E {
 	t := reflect.TypeFor[E]()
-	if t.Kind() == reflect.Ptr {
+	switch t.Kind() {
+	case reflect.Ptr:
 		rType := t.Elem()
 		z := reflect.New(rType).Interface()
 		return z.(E)
+	default:
+		return *new(E)
 	}
-	return *new(E)
 }
